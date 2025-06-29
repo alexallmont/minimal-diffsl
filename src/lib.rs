d6e6072368d524ca19ef5aec1ad43ee5d304c7fa
Alex Allmont <alex.allmont@dtc.ox.ac.uk>
2025-06-29 18:04:45 +0000

    feat: experimental 2D mat code for discussion
    - WIP adding this example after trying several approaches to convert nalgebra (and ideally also faer using same API), but I can't find a direct method for getting data from diffsol into an Array2 that can then be used to pass on into a PyArray2. Committing example for further discussion.

--- src/lib.rs
+++ src/lib.rs
@@ -53,11 +53,23 @@ fn make_numpy_array_from_raw<'py>(py: Python<'py>) -> Bound<'py, PyArray1<f64>> 
 }
 
 #[pyfunction]
-pub fn solver_test<'py>(py: Python<'py>) -> Bound<'py, PyArray1<f64>> {
+pub fn solver_test<'py>(py: Python<'py>) -> (Bound<'py, PyArray2<f64>>, Bound<'py, PyArray1<f64>>) {
     let problem = problem_implicit();
     let mut solver = problem.bdf::<LS>().unwrap();
-    let (_ys, ts): (M, Vec<T>) = solver.solve(10.0).unwrap();
-    PyArray1::from_owned_array(py, ts.into())
+    let (ys, ts): (M, Vec<T>) = solver.solve(10.0).unwrap();
+
+    // FIXME experiment attempt to get ptr for creating Array2/PyArray2
+    let shape = ys.inner().shape();
+    // let ptr = ys.inner().as_ptr();
+    // let len = shape.0 * shape.1;
+    // let vec = unsafe { Vec::<f64>::from_raw_parts(ptr, len, len) };
+    // let ys_arr = unsafe { Array2::from_shape_vec(shape, vec) };
+    let ts_arr = Array1::from(ts);
+    (
+        //PyArray2::from_owned_array(py, ys_arr.unwrap()),
+        unsafe { PyArray2::new(py, shape, false) }, // FIXME init from data
+        PyArray1::from_owned_array(py, ts_arr)
+    )
 }
 
 #[pymodule]
